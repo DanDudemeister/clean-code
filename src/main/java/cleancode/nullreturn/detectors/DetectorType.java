856e6a6cfe7e26f761570f6d3f60c7fe0d06d723
Dan <DanDudemeister@users.noreply.github.com>
2018-03-24 22:40:47 +0100

    added base classes for null-detector-mechanism

---
+++ src/main/java/cleancode/nullreturn/detectors/DetectorType.java
@@ -0,0 +1,6 @@
+package cleancode.nullreturn.detectors;
+
+public enum DetectorType {
+    ASSIGNMENT,
+    RETURN_STATEMENT
+}
---
+++ src/main/java/cleancode/nullreturn/detectors/NullReturnDetector.java
@@ -0,0 +1,29 @@
+package cleancode.nullreturn.detectors;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+public interface NullReturnDetector {
+
+    boolean possiblyReturnsNull();
+
+    default LocalQuickFix getFix() {
+        //TODO only temporal; will be replaced with concrete implementations later
+        return new LocalQuickFix() {
+            @Nls
+            @NotNull
+            @Override
+            public String getFamilyName() {
+                return "family name";
+            }
+
+            @Override
+            public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
+
+            }
+        };
+    };
+}
