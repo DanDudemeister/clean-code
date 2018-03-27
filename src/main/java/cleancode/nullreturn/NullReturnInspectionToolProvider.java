efea7165ab74bb0a07c9bd900613f1dcaa5e7b62
Dan <DanDudemeister@users.noreply.github.com>
2018-03-27 21:52:19 +0200

    cleaned up code

--- src/main/java/cleancode/nullreturn/NullReturnInspectionToolProvider.java
+++ src/main/java/cleancode/nullreturn/NullReturnInspectionToolProvider.java
@@ -1,8 +1,10 @@
 package cleancode.nullreturn;
 
 import com.intellij.codeInspection.InspectionToolProvider;
+import org.jetbrains.annotations.NotNull;
 
 public class NullReturnInspectionToolProvider implements InspectionToolProvider {
+    @NotNull
     public Class[] getInspectionClasses() {
         return new Class[]{NullReturnInspectionTool.class};
     }
