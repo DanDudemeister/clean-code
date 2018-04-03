010a7dad736cb87958f3f5d15dee2c2996956590
Dan <DanDudemeister@users.noreply.github.com>
2018-04-03 16:54:54 +0200

    implemented OptionalQuickFixHandling for Java8-Method-References + some code cleanup

--- src/main/java/cleancode/nullreturn/quickfixes/implementations/optional/OptionalQuickFix.java
+++ src/main/java/cleancode/nullreturn/quickfixes/implementations/optional/OptionalQuickFix.java
@@ -1,7 +1,5 @@
 package cleancode.nullreturn.quickfixes.implementations.optional;
 
-import cleancode.utils.Constants;
-import cleancode.utils.Constants.Types;
 import cleancode.utils.PsiUtils;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
@@ -15,12 +13,14 @@ import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiLocalVariable;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.PsiMethodReferenceExpression;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiReferenceExpression;
 import com.intellij.psi.PsiReturnStatement;
 import com.intellij.psi.PsiType;
 import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.refactoring.util.LambdaRefactoringUtil;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -35,7 +35,13 @@ import org.jetbrains.annotations.NotNull;
 public class OptionalQuickFix implements LocalQuickFix {
 
     private static final String QUICK_FIX_NAME = "Replace null with optional";
-    private static final String OF_NULLABLE_TEMPLATE = "java.util.Optional.ofNullable(" + Constants.PLACEHOLDER + ")";
+    private static final String PLACEHOLDER = "{0}";
+    private static final String JAVA_UTIL_OPTIONAL_FULL_QUALIFIED_REGEX = "^(java.util.Optional<).*(>)$";
+    private static final String JAVA_UTIL_OPTIONAL_SHORTENED_REGEX = "^(Optional<).*(>)$";
+    private static final String OF_NULLABLE_TEMPLATE = "java.util.Optional.ofNullable(" + PLACEHOLDER + ")";
+    private static final String OPTIONAL_TEMPLATE = "Optional<" + PLACEHOLDER + ">";
+    private static final String OR_ELSE_NULL = ".orElse(null)";
+
 
     @Nls
     @NotNull
@@ -44,12 +50,14 @@ public class OptionalQuickFix implements LocalQuickFix {
         return QUICK_FIX_NAME;
     }
 
+
     @Nls
     @NotNull
     @Override
     public String getFamilyName() {
         return getName();
     }
+
 
     @Override
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
@@ -61,6 +69,10 @@ public class OptionalQuickFix implements LocalQuickFix {
         replaceAllReturnedValuesWithOptionalIfNecessary(project, assignedOrReturnedValues);
 
         Collection<PsiReference> usagesOfMethod = PsiUtils.findUsagesOfMethod(surroundingMethod);
+        List<PsiMethodReferenceExpression> java8MethodReferences = findJava8MethodReferences(usagesOfMethod);
+        convertMethodReferencesToLambda(java8MethodReferences);
+
+        usagesOfMethod = PsiUtils.findUsagesOfMethod(surroundingMethod);
         adaptUsagesIfNecessary(project, usagesOfMethod);
 
         // add "generated" TODOs
@@ -73,8 +85,7 @@ public class OptionalQuickFix implements LocalQuickFix {
     }
 
 
-    private List<PsiExpression> getAssignedOrReturnedValues(
-        Collection<PsiReturnStatement> allReturnStatements) {
+    private List<PsiExpression> getAssignedOrReturnedValues(Collection<PsiReturnStatement> allReturnStatements) {
         return allReturnStatements.stream()
             .map(PsiUtils::getAssignedOrReturnedExpressionFromElement)
             .map(optionalPsiExpression -> optionalPsiExpression.orElse(null))
@@ -83,11 +94,17 @@ public class OptionalQuickFix implements LocalQuickFix {
     }
 
 
+    private void convertMethodReferencesToLambda(List<PsiMethodReferenceExpression> java8MethodReferences) {
+        java8MethodReferences.forEach(psiReference -> LambdaRefactoringUtil.convertMethodReferenceToLambda(psiReference, true, true));
+    }
+
+
+    //TODO Optional.empty(), if null! (instead of Optional.ofNullable(null))
     private void replaceAllReturnedValuesWithOptionalIfNecessary(@NotNull Project project, List<PsiExpression> assignedOrReturnedValues) {
         assignedOrReturnedValues.forEach(returnedValue -> {
-            if (returnedValue.getType() != null && !returnedValue.getType().getCanonicalText().matches(Types.JAVA_UTIL_OPTIONAL)) {
+            if (returnedValue.getType() != null && !returnedValue.getType().getCanonicalText().matches(JAVA_UTIL_OPTIONAL_FULL_QUALIFIED_REGEX)) {
                 String returnedValueAsText = returnedValue.getText();
-                returnedValueAsText = OF_NULLABLE_TEMPLATE.replace(Constants.PLACEHOLDER, returnedValueAsText);
+                returnedValueAsText = OF_NULLABLE_TEMPLATE.replace(PLACEHOLDER, returnedValueAsText);
                 PsiExpression newReturnedValue = SERVICE.getInstance(project).createExpressionFromText(returnedValueAsText, null);
                 PsiElement newReturnedValueWithImport = PsiUtils.replaceFullyQualifiedNameWithImport(newReturnedValue, project);
                 returnedValue.replace(newReturnedValueWithImport);
@@ -96,71 +113,25 @@ public class OptionalQuickFix implements LocalQuickFix {
     }
 
 
+    private List<PsiMethodReferenceExpression> findJava8MethodReferences(Collection<PsiReference> usagesOfMethod) {
+        return usagesOfMethod.stream()
+            .filter(usage -> usage instanceof PsiMethodReferenceExpression)
+            .map(usage -> (PsiMethodReferenceExpression) usage)
+            .collect(Collectors.toList());
+    }
+
+
     private void adaptUsagesIfNecessary(@NotNull Project project, Collection<PsiReference> usagesOfMethod) {
         usagesOfMethod.forEach(psiReference -> {
-            //TODO: Methodenreferenz in Lambda anpassen!!
-
             PsiElement parentElementWhichContainsMethodCallChain = getParentElementWhichContainsMethodCallChain(psiReference);
-            String currentlyCalledMethodName = getNameOfCurrentlyCalledMethod(psiReference.getElement());
+            String currentlyCalledMethodName = getNameOfCurrentlyCalledMethod(psiReference.getElement(), project);
 
-            boolean onRightSideOfAnAssignment = isOnRightSideOfAnAssignment(psiReference.getElement());
-            boolean adaptUsage;
-
-            if (onRightSideOfAnAssignment) {
-                adaptUsage = !isAlreadyAssignedAsOptional(psiReference) && !doesMethodNameMatchAnyOfOptionalClassMethodNames(currentlyCalledMethodName);
-            } else {
-                adaptUsage = !doesMethodNameMatchAnyOfOptionalClassMethodNames(currentlyCalledMethodName);
-            }
+            boolean adaptUsage = isAdaptionOfUsageNecessary(psiReference, currentlyCalledMethodName);
 
             if (adaptUsage) {
-                String methodCallAsText = parentElementWhichContainsMethodCallChain.getText();
-                methodCallAsText = methodCallAsText + ".orElse(null)";
-                PsiExpression newReturnedValue = SERVICE
-                    .getInstance(project).createExpressionFromText(methodCallAsText, null);
-                PsiElement newMethodCall = PsiUtils
-                    .replaceFullyQualifiedNameWithImport(newReturnedValue, project);
-                parentElementWhichContainsMethodCallChain.replace(newMethodCall);
+                replaceUsage(project, parentElementWhichContainsMethodCallChain);
             }
         });
-    }
-
-
-    private boolean isOnRightSideOfAnAssignment(PsiElement element) {
-        PsiLocalVariable localVariable = null;
-        PsiAssignmentExpression assignment = null;
-
-        localVariable = PsiTreeUtil.getParentOfType(element, PsiLocalVariable.class, true, PsiCodeBlock.class);
-
-        if (localVariable == null) {
-            assignment = PsiTreeUtil.getParentOfType(element, PsiAssignmentExpression.class, true, PsiCodeBlock.class);
-        }
-
-        return localVariable != null || assignment != null;
-    }
-
-
-    //TODO vielleicht besserer Algorithmus nötig, der nicht jeden Charakter betrachtet, sondern auf Typen prüft
-    private String getNameOfCurrentlyCalledMethod(PsiElement psiElement) {
-        String currentlyCalledMethodName = "";
-        PsiElement currentElement = PsiTreeUtil.nextLeaf(psiElement);
-
-        while (!")".equals(currentElement.getText())) {
-            currentElement = PsiTreeUtil.nextLeaf(currentElement);
-        }
-
-        PsiElement closingBrackets = currentElement;
-        PsiElement elementAfterClosingBrackets = PsiTreeUtil.nextLeaf(closingBrackets);
-
-        while (elementAfterClosingBrackets instanceof PsiWhiteSpace) {
-            elementAfterClosingBrackets = PsiTreeUtil.nextLeaf(elementAfterClosingBrackets);
-        }
-
-        if (".".equals(elementAfterClosingBrackets.getText())) {
-            //TODO Der Fall a. \n orElse() geht noch nicht!
-            currentlyCalledMethodName = PsiTreeUtil.nextLeaf(PsiTreeUtil.nextLeaf(elementAfterClosingBrackets)).getText();
-        }
-
-        return currentlyCalledMethodName;
     }
 
 
@@ -175,9 +146,64 @@ public class OptionalQuickFix implements LocalQuickFix {
     }
 
 
+    private String getNameOfCurrentlyCalledMethod(PsiElement psiElement, Project project) {
+        String currentlyCalledMethodName = "";
+
+        PsiElement currentElement = PsiTreeUtil.nextLeaf(psiElement);
+
+        while (!")".equals(currentElement.getText())) {
+            currentElement = PsiTreeUtil.nextLeaf(currentElement);
+        }
+
+        PsiElement closingBrackets = currentElement;
+        PsiElement elementAfterClosingBrackets = PsiTreeUtil.nextLeaf(closingBrackets);
+
+        while (elementAfterClosingBrackets instanceof PsiWhiteSpace) {
+            elementAfterClosingBrackets = PsiTreeUtil.nextLeaf(elementAfterClosingBrackets);
+        }
+
+        if (".".equals(elementAfterClosingBrackets.getText())) {
+            //TODO Doesn't work if there's a new line after the dot (a. \n orElse())
+            currentlyCalledMethodName = PsiTreeUtil.nextLeaf(PsiTreeUtil.nextLeaf(elementAfterClosingBrackets)).getText();
+        }
+
+        return currentlyCalledMethodName;
+    }
+
+
+    //TODO: this logic doesn't work for all cases:
+    // actually it needs to be determined what the place where the method is
+    // called expects the method's return type to be
+    // which is really not that trivial especially in Java 8 streams
+    private boolean isAdaptionOfUsageNecessary(PsiReference psiReference, String currentlyCalledMethodName) {
+        boolean adaptUsage;
+        boolean onRightSideOfAnAssignment = isOnRightSideOfAnAssignment(psiReference.getElement());
+
+        if (onRightSideOfAnAssignment) {
+            adaptUsage = !isAlreadyAssignedAsOptional(psiReference) && !doesMethodNameMatchAnyOfOptionalClassMethodNames(currentlyCalledMethodName);
+        } else {
+            adaptUsage = !doesMethodNameMatchAnyOfOptionalClassMethodNames(currentlyCalledMethodName);
+        }
+
+        return adaptUsage;
+    }
+
+
+    private boolean isOnRightSideOfAnAssignment(PsiElement element) {
+        PsiAssignmentExpression assignment = null;
+        PsiLocalVariable localVariable = PsiTreeUtil.getParentOfType(element, PsiLocalVariable.class, true, PsiCodeBlock.class);
+
+        if (localVariable == null) {
+            assignment = PsiTreeUtil.getParentOfType(element, PsiAssignmentExpression.class, true, PsiCodeBlock.class);
+        }
+
+        return localVariable != null || assignment != null;
+    }
+
+
     private boolean isAlreadyAssignedAsOptional(PsiReference psiReference) {
         PsiType type = getTypeOfAssignedVariableFromElementRecursively(((PsiReferenceExpression) psiReference).getParent());
-        return type.getCanonicalText().matches(Types.JAVA_UTIL_OPTIONAL);
+        return type.getCanonicalText().matches(JAVA_UTIL_OPTIONAL_FULL_QUALIFIED_REGEX);
     }
 
 
@@ -213,10 +239,20 @@ public class OptionalQuickFix implements LocalQuickFix {
     }
 
 
+    private void replaceUsage(@NotNull Project project, PsiElement parentElementWhichContainsMethodCallChain) {
+        String methodCallAsText = parentElementWhichContainsMethodCallChain.getText();
+        methodCallAsText = methodCallAsText + OR_ELSE_NULL;
+        PsiExpression newReturnedValue = SERVICE.getInstance(project).createExpressionFromText(methodCallAsText, null);
+        PsiElement newMethodCall = PsiUtils.replaceFullyQualifiedNameWithImport(newReturnedValue, project);
+        parentElementWhichContainsMethodCallChain.replace(newMethodCall);
+    }
+
+
     private void adaptReturnTypeIfNecessary(@NotNull Project project, PsiMethod surroundingMethod) {
         String oldTypeAsText = surroundingMethod.getReturnTypeElement().getType().getPresentableText();
-        if (!oldTypeAsText.matches("^(Optional<).*(>)$")) {
-            PsiType newPsiType = SERVICE.getInstance(project).createTypeByFQClassName("Optional<" + oldTypeAsText + ">");
+        if (!oldTypeAsText.matches(JAVA_UTIL_OPTIONAL_SHORTENED_REGEX)) {
+            String newTypeAsText = OPTIONAL_TEMPLATE.replace(PLACEHOLDER, oldTypeAsText);
+            PsiType newPsiType = SERVICE.getInstance(project).createTypeByFQClassName(newTypeAsText);
             PsiElement newReturnType = SERVICE.getInstance(project).createTypeElement(newPsiType);
             surroundingMethod.getReturnTypeElement().replace(newReturnType);
         }
